6d1a62b3c3067c6a1561e10c896060b90a1ca252
Mayank Raj <65666477+mankuuu@users.noreply.github.com>
2023-02-20 18:47:56 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define mi map<int>
+#define pub push_back
+#define si set<int>
+#define cy cout<<"YES\n";
+#define cn cout<<"NO\n";
+
+
+void solve()
+{
+    
+}
+
+//Main Function
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int test;
+    cin >> test;
+    while(test--)
+    {
+    solve();
+    }
+}
