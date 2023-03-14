42aa3daed112aad12fa792dd7ef5ae6950c27663
Mayank Raj <65666477+mankuuu@users.noreply.github.com>
2023-03-15 01:40:53 +0530

    Create Sieve_of_Eratosthenes.cpp

---
+++ Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,16 @@
+void sieve(int n)
+{
+	vector <bool> isPrime(n+1, true);
+	for(int i=2; i<=n; i++)
+	{
+		if(isPrime[i])
+		{
+			cout << i << " ";
+			for(int j=i*i; j<=n; j+=i)
+			{
+				isPrime[j] = false;
+			}
+		}
+	}
+}
+				
