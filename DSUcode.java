923769437fba14ec8b6233d66b22093d985eed19
Mayank Raj <65666477+mankuuu@users.noreply.github.com>
2024-10-19 00:42:23 +0530

    Update DSUcode.java

--- DSUcode.java
+++ DSUcode.java
@@ -3,11 +3,13 @@ class DisjointSet {
     //remember to import java.util.*;
     List<Integer> rank = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
 
     public DisjointSet(int n) {
         for(int i = 0 ; i <= n ; i++) {
             rank.add(0);
             parent.add(i);
+            size.add(1);
         }
     }
 
@@ -33,5 +35,23 @@ class DisjointSet {
             int rankU = rank.get(ulp_u);
             rank.set(ulp_u , rnakU + 1);
         }
+    }
+
+    public void unionBySize(int u , int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(size.get(ulp_u) < size.get(ulp_v) {
+            parent.set(ulp_u , ulp_v);
+            int inisize = size.get(ulp_v);
+            int toadd = size.get(ulp_u);
+            size.set(ulp_v , inisize + toadd);
+        } else {
+            parent.set(ulp_v , ulp_u);
+            int inisize = size.get(ulp_u);
+            int toadd = size.get(ulp_v);
+            size.set(ulp_u , inisize + toadd);
+        } 
+        
     }
 }
